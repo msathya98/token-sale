a43b37b533ebdbb8ba62686238299044334feadb
Sathya M <62427113+msathya98@users.noreply.github.com>
2021-02-17 19:41:41 +0530

    DappToken.sol with transfer and approve functions.

--- DappToken.sol
+++ DappToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.7.4;
 
 contract DappToken {
     string public name = "DApp Token";
@@ -7,16 +7,56 @@ contract DappToken {
     string public standard = "DApp Token v1.0";
     uint256 public totalSupply;
 
-   mapping(address => uint256) public balanceOf;
+event Transfer(
+    address indexed _from,
+    address indexed _to,
+    uint256 _value
+);
 
-    constructor(uint256 _initialSupply) {
+event Approval(
+    address indexed _owner,
+    address indexed _spender,
+    uint256 _value
+
+);
+mapping(address => uint256) public balanceOf;
+  mapping(address => mapping(address => uint256)) public allowance;
+
+    constructor(uint256 _initialSupply)  {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
         
     }
 
-    function transfer ( address _to, uint256 _value) public view returns (bool success)  {
+    function transfer( address _to, uint256 _value) public returns (bool success)  {
      require(balanceOf[msg.sender] >= _value);
+
+     balanceOf[msg.sender] -= _value;
+     balanceOf[_to] += _value;
+
+      emit Transfer(msg.sender, _to, _value);
+     return true;
         
+    }
+
+    function  approve(address _spender, uint256 _value) public returns (bool success) {
+   allowance[msg.sender][_spender] = _value;
+
+   Approval(msg.sender, _spender, _value);  
+   return true;   
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender] -= _value;
+
+        Transfer(_from, _to, _value);
+
+        return true;
     }
 }
