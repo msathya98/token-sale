c7390a27bf916af1ef5eeb09387674b1b7e68c22
Sathya M <62427113+msathya98@users.noreply.github.com>
2021-02-17 19:43:03 +0530

    DappToken.js of smart-token  for testing different cases.

--- test/DappToken.js
+++ test/DappToken.js
@@ -1,5 +1,3 @@
-const { assert } = require("console");
-
 var DappToken = artifacts.require("./DappToken.sol");
 
 contract('DappToken', function(accounts) 
@@ -15,9 +13,9 @@ contract('DappToken', function(accounts)
      return tokenInstance.symbol();
    }).then(function(symbol){
      assert.equal(symbol, "DAPP", "has the correct symbol");
-
+        return tokenInstance.standard();
    }).then(function(standard){
-     assert.equal(standard, "DAPP token v1.0", "has the correct standard");
+     assert.equal(standard, "DApp Token v1.0", "has the correct standard");
    });
  })
 
@@ -40,19 +38,86 @@ contract('DappToken', function(accounts)
     return DappToken.deployed().then(function(instance){
       tokenInstance = instance;
 
-      return tokenInstance.transfer.call(accounts[1], 99999999999999999999);
+      return tokenInstance.transfer.call(accounts[1], 99999999999);
 
     }).then(assert.fail).catch(function(error){
       assert(error.message.indexOf('revert') >= 0, "error message must contain revert");
-      return tokenInstance.transfer(accounts[1], 25000, { from: accounts[0] } );
-
+      return tokenInstance.transfer(accounts[1], 2500, { from: accounts[0] } );
     }).then(function(receipt) {
+      assert.equal(receipt.logs.length, 1, "triggers one event ");
+      assert.equal(receipt.logs[0].event, "Transfer", 'should be the "transfer" event');
+      assert.equal(receipt.logs[0].args._from, accounts[0], "logs the account the tokens are transferred from");
+      assert.equal(receipt.logs[0].args._to, accounts[1], "logs the account the tokens are transferred to")
+      assert.equal(receipt.logs[0].args._value, 2500000, "logs the transfer amount");
       return tokenInstance.balanceOf(accounts[1]); 
     }).then(function(balance){
-      assert.equal(balance.toNumberO(), 2500000, "adds the amount to the recieving account");
+      assert.equal(balance.toNumber(), 2500, "adds the amount to the recieving account");
       return tokenInstance.balanceOf(accounts[0]);
     }).then(function(balance){
-      assert.equal(balance.toNumber(), 7500000, "deduct the amount from the sending account")
+      assert.equal(balance.toNumber(), 997500, "deduct the amount from the sending account")
     })
-  })
+  });
+
+  it('approves tokens for delegated transfer', function() {
+    return DappToken.deployed().then(function(instance) {
+      tokenInstance = instance;
+      return tokenInstance.approve.call(accounts[1], 100);
+    }).then(function(success) {
+      assert.equal(success, true, 'it returns true');
+      return tokenInstance.approve(accounts[1], 100, { from: accounts[0] });
+    }).then(function(receipt) {
+      assert.equal(receipt.logs.length, 1, 'triggers one event');
+      assert.equal(receipt.logs[0].event, 'Approval', 'should be the "Approval" event');
+      assert.equal(receipt.logs[0].args._owner, accounts[0], 'logs the account the tokens are authorized by');
+      assert.equal(receipt.logs[0].args._spender, accounts[1], 'logs the account the tokens are authorized to');
+      assert.equal(receipt.logs[0].args._value, 100, 'logs the transfer amount');
+      return tokenInstance.allowance(accounts[0], accounts[1]);
+    }).then(function(allowance) {
+      assert.equal(allowance.toNumber(), 100, 'stores the allowance for delegated trasnfer');
+    });
+  });
+  
+
+  it('handles delegated token transfers', function() {
+    return DappToken.deployed().then(function(instance) {
+      tokenInstance = instance;
+      fromAccount = accounts[2];
+      toAccount = accounts[3];
+      spendingAccount = accounts[4];
+      // Transfer some tokens to fromAccount
+      return tokenInstance.transfer(fromAccount, 100, { from: accounts[0] });
+    }).then(function(receipt) {
+      // Approve spendingAccount to spend 10 tokens form fromAccount
+      return tokenInstance.approve(spendingAccount, 10, { from: fromAccount });
+    }).then(function(receipt) {
+      // Try transferring something larger than the sender's balance
+      return tokenInstance.transferFrom(fromAccount, toAccount, 9999, { from: spendingAccount });
+    }).then(assert.fail).catch(function(error) {
+      assert(error.message.indexOf('revert') >= 0, 'cannot transfer value larger than balance');
+      // Try transferring something larger than the approved amount
+      return tokenInstance.transferFrom(fromAccount, toAccount, 20, { from: spendingAccount });
+    }).then(assert.fail).catch(function(error) {
+      assert(error.message.indexOf('revert') >= 0, 'cannot transfer value larger than approved amount');
+      return tokenInstance.transferFrom.call(fromAccount, toAccount, 10, { from: spendingAccount });
+    }).then(function(success) {
+      assert.equal(success, true);
+      return tokenInstance.transferFrom(fromAccount, toAccount, 10, { from: spendingAccount });
+    }).then(function(receipt) {
+      assert.equal(receipt.logs.length, 1, 'triggers one event');
+      assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
+      assert.equal(receipt.logs[0].args._from, fromAccount, 'logs the account the tokens are transferred from');
+      assert.equal(receipt.logs[0].args._to, toAccount, 'logs the account the tokens are transferred to');
+      assert.equal(receipt.logs[0].args._value, 10, 'logs the transfer amount');
+      return tokenInstance.balanceOf(fromAccount);
+    }).then(function(balance) {
+      assert.equal(balance.toNumber(), 90, 'deducts the amount from the sending account');
+      return tokenInstance.balanceOf(toAccount);
+    }).then(function(balance) {
+      assert.equal(balance.toNumber(), 10, 'adds the amount from the receiving account');
+      return tokenInstance.allowance(fromAccount, spendingAccount);
+    }).then(function(allowance) {
+      assert.equal(allowance.toNumber(), 0, 'deducts the amount from the allowance');
+    });
+  });
+
 })
